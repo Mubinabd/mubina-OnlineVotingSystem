0e4bb6caa3313871200fe7018fc241a357f26e91
Mubina <153791173+Mubinabd@users.noreply.github.com>
2024-06-07 15:42:41 +0500

    Delete voting-system-service directory

--- voting-system-service/migrations/000001_create_table.down.sql
+++
@@ -1,6 +0,0 @@
-drop table if EXISTS party;
-drop table if EXISTS public;
-drop table if EXISTS votes;
-drop table if EXISTS public_vote;
-drop table if EXISTS candidate;
-drop table if EXISTS election;
