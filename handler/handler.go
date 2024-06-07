888e270a293e9ad55d50888978c47f7967af7353
Mubinabd <bahodirovamubina@gmail.com>
2024-06-07 16:13:06 +0500

    apigateway

---
+++ handler/handler.go
@@ -0,0 +1,21 @@
+package handler
+
+import (
+	pb "project/genproto/public"
+
+	"google.golang.org/grpc"
+)
+
+type HandlerStruct struct {
+	Candidate  pb.CandidateServiceClient
+	Election  pb.ElectionServiceClient
+	PublicVote pb.PublicVoteServiceClient
+}
+
+func NewHandlerStruct(connClient *grpc.ClientConn) *HandlerStruct {
+	return &HandlerStruct{
+		Election:   pb.NewElectionServiceClient(connClient),
+		Candidate:  pb.NewCandidateServiceClient(connClient),
+		PublicVote: pb.NewPublicVoteServiceClient(connClient),
+	}
+}
