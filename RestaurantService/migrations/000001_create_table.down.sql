b1eed8728aedf7b706aa63a708a5d3ddd82ef691
Mubina <153791173+Mubinabd@users.noreply.github.com>
2024-06-11 17:37:02 +0500

    Delete RestaurantService directory

--- RestaurantService/migrations/000001_create_table.down.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE if exists reservationsorders;
-DROP TABLE if exists menu;
-DROP TABLE if exists reservations;
-DROP TABLE if exists restaurants
-DROP TABLE if exists users;
--- RestaurantService/service/service.go
+++
@@ -1,9 +0,0 @@
-package service
-
-type Service struct {
-	RS  *RestaurantService
-}
-
-func NewService(rs *RestaurantService) *Service {
-	return &Service{RS: rs}
-}
